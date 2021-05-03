f60803bbb9ebc2197913e7cd5e035a82f384a815
Siyovush Kadyrov <siyovush.kadyrov@gmail.com>
2021-05-03 22:34:14 +0300

    Fix unsplash key being in repository

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import unsplashApiKey from '../keys/unsplash.js';
 
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization:
-          "Client-ID 7a398622b38cfa7d2ebc8fed5e561ee602f9d0a15f258983c992f6ab5e8c42af"
+          `Client-ID ${unsplashApiKey}`
       }
 });
