df7f56ae797d9ae5672149a07b8066a428669038
Siyovush Kadyrov <siyovush.kadyrov@gmail.com>
2021-05-08 23:28:17 +0300

    Added list photos for topic endpoint

--- routes/search.js
+++ routes/search.js
@@ -23,7 +23,20 @@ async function listTopics()
       per_page: unsplashConfigs.perPage,
       order_by: unsplashConfigs.orderBy
     }
-});
+  });
+}
+
+async function getPhotosForTopic(id_or_slug)
+{
+  return await unsplash.get(`/topics/${id_or_slug}/photos`, {
+    params: 
+    {
+      id_or_slug: id_or_slug,
+      page: unsplashConfigs.pageNumber,
+      per_page: unsplashConfigs.perPage,
+      order_by: unsplashConfigs.orderBy
+    }
+  });
 }
 
 module.exports = (app) => {
@@ -34,6 +47,11 @@ module.exports = (app) => {
 
   app.get('/topics', async(req, res) => {
     const response = await listTopics();
+    res.send(response.data);
+  });
+
+  app.get('/topics/photos', async(req, res) => {
+    const response = await getPhotosForTopic(req.query.id_or_slug);
     res.send(response.data);
   });
 }
